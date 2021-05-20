84b9a0ed2583e37d1d6180fdc39b43fd88dfa652
decorb_n <decorb_n@etna-alternance.net>
2021-05-20 20:32:30 +0200

    fix(script): SQL synthax error

--- postgres/users-table.sh
+++ postgres/users-table.sh
@@ -6,7 +6,7 @@ CREATE TABLE users (
   id INT PRIMARY KEY NOT NULL,
   nom VARCHAR(100),
   prenom VARCHAR(100),
-  email VARCHAR(255),
+  email VARCHAR(255)
 );
 INSERT INTO users VALUES (1, 'decorbez', 'nicolas', 'decorbezpro@gmail.com');
 EOSQL
