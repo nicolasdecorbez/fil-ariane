88ff9a3b5c394097dd783da52891ac454cb7f364
decorb_n <decorb_n@etna-alternance.net>
2021-05-21 12:28:55 +0200

    feat(sql): Add columns to users table

--- postgres/users-table.sh
+++ postgres/users-table.sh
@@ -4,8 +4,10 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
+  username VARCHAR(100),
+  name VARCHAR(100),
+  firstname VARCHAR(100),
+  phone VARCHAR(10),
   email VARCHAR(255)
 );
 INSERT INTO users (nom, prenom, email) VALUES ('decorbez', 'nicolas', 'decorbezpro@gmail.com');
