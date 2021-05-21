f132f29ef4daf4ec6f28a46050eba031ca7e02e8
decorb_n <decorb_n@etna-alternance.net>
2021-05-21 12:03:08 +0200

    feat(sql): Auto-increment parameter for ID column

--- postgres/test.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users {
-  id INT PRIMARY KEY NOT NULL,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  email VARCHAR(255),
-}
-
-INSERT INTO users VALUES (1, 'decorbez', 'nicolas', 'decorbezpro@gmail.com');
