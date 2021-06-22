185af45d442e65aacf8102fb31dc3c732c284345
GrosduboisBen <72658234+GrosduboisBen@users.noreply.github.com>
2021-06-22 20:15:08 +0200

    feat: View pager and slides for Homepage

--- app/src/main/java/com/example/ardianethread/MainActivity.kt
+++ app/src/main/java/com/example/ardianethread/MainActivity.kt
@@ -5,11 +5,35 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.widget.Toolbar
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        var pager2 = findViewById<ViewPager2>(R.id.view_pager2)
+        var adapter = FragmentAdapter(supportFragmentManager,lifecycle)
+
+        pager2.adapter= adapter
+
+        tabLayout.addTab(tabLayout.newTab().setText("Ariane"))
+        tabLayout.addTab(tabLayout.newTab().setText("Theseus"))
+
+        TabLayoutMediator(tabLayout,pager2){tab,position->
+            when(position) {
+                0 ->{
+                    tab.text = "Ariane"
+                }
+                1->{
+                    tab.text = "Theseus"
+                }
+            }
+        }.attach()
         setSupportActionBar(findViewById(R.id.aToolbar))
         title = ""
     }
---
+++ app/src/main/java/com/example/ardianethread/fragment_ariane.kt
@@ -0,0 +1,55 @@
+package com.example.ardianethread
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [fragment_ariane.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class fragment_ariane : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ariane, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment fragment_ariane.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            fragment_ariane().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
