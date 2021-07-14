7e88b0defa9e22723d3c6e993fad584ac566a788
GrosduboisBen <72658234+GrosduboisBen@users.noreply.github.com>
2021-07-14 13:22:55 +0200

    feat/ Add Alarm Settings View

---
+++ app/src/main/java/com/example/ardianethread/AlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ardianethread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Alarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
