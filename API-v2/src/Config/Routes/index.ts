8fd1fb3874cb2db69e29644ca3885503123ec2c6
nikko-regolith <decorb_n@etna-alternance.net>
2021-07-29 21:06:30 +0200

    feat(db): add Message controller support

--- API-v2/src/Config/Routes/index.ts
+++ API-v2/src/Config/Routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import UserRouter from "./UserRouter"
+import MessageRouter from "./MessageRouter"
 
 const router = express.Router()
 router.use("/users", UserRouter)
+router.use("/messages", MessageRouter)
 
 export default router
