32871fcaf28ca93587ba48ae39b2acaed189e11c
nikko-regolith <decorb_n@etna-alternance.net>
2021-08-01 16:43:35 +0200

    docs(repo): add repository documentation

--- API-v2/src/Config/Repositories/MessageRepo.ts
+++ API-v2/src/Config/Repositories/MessageRepo.ts
@@ -1,20 +1,29 @@
 import { getRepository } from "typeorm"
 import { MessageModel } from "../Models"
 
-// Payload definition for POST and PUT methods.
+/**
+ *  payload definition for POST and PUT methods
+ */
 export interface MessageSchema {
   senderId: number
   receiverId: number
   messageContent: string
 }
 
-// GET All messages
+/**
+ *  [async function to retrieve all messages]
+ *  @return [a promise with an array of MessageModel]
+ */
 export const getAllMessages = async (): Promise<Array<MessageModel>> => {
   const messageRepository = getRepository(MessageModel)
   return messageRepository.find()
 }
 
-// POST new message
+/**
+ *  [async function to create a new message]
+ *  @param  request [MessageSchema with data]
+ *  @return         [a promise with the created message]
+ */
 export const createMessage = async (request: MessageSchema): Promise<MessageModel> => {
   const messageRepository = getRepository(MessageModel)
   const message = new MessageModel()
