e0f323abedb7325c87084ae4cd7cc8893d43ade8
nikko-regolith <decorb_n@etna-alternance.net>
2021-09-04 00:56:24 +0200

    feat(components): add parents components for each part of the app

---
+++ api-dashboard/src/app/four-oh-four/four-oh-four.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-four-oh-four',
+  templateUrl: './four-oh-four.component.html',
+  styleUrls: ['./four-oh-four.component.sass']
+})
+export class FourOhFourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
