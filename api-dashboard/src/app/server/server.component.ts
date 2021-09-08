60e82dd96e8e746b7fb82fe12ffd8e68d6932097
nikko-regolith <decorb_n@etna-alternance.net>
2021-09-08 04:56:51 +0200

    feat(server): add server view to test server uptime

--- api-dashboard/src/app/server/server.component.ts
+++ api-dashboard/src/app/server/server.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
+
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.sass']
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private db_service: DatabaseService,
+  ) { }
+
+  get serverStatusCode() { return this.db_service.status }
+  isServerUp = true; 
 
   ngOnInit(): void {
+    this.checkServer()
+    if (this.serverStatusCode >= 400 ) {
+      this.isServerUp = false;
+    }
+  }
+
+  checkServer() {
+    this.db_service.health_check();
   }
 
 }
